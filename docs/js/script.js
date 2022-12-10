ae0cc564467c30b36138a33bdd57176a2ff1a2ac
Bohdan <example@gmail.com>
2022-12-10 05:48:06 +0200

    adaptive1

--- docs/js/script.js
+++ docs/js/script.js
@@ -9,3 +9,10 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+const counters = document.querySelectorAll('.progress-counter'),
+    lines = document.querySelectorAll('.progress-line span');
+
+counters.forEach( (item, i) =>{
+    lines[i].style.width = item.innerHTML;
+});
